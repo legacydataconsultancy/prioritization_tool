c5e582b75c389bcc058dffe25633d7864ebf5643
legacydataconsultancy <legacydataconsultancy@gmail.com>
2025-06-13 23:55:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # <-- added for CORS
 import joblib
 
 app = Flask(__name__)
+CORS(app)  # <-- enable CORS for all routes
 
 # Load trained model
 model = joblib.load('logistic_regression_model.joblib')
